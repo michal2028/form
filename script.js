2a0c1ebe31f94184bfaa21662f41a229a6cfc5e1
Michal <m.nowak.kontakt@onet.eu>
2023-06-03 20:18:52 +0200

    validate update

--- script.js
+++ script.js
@@ -1,21 +1,19 @@
-
-
 function queryInvalid() {
   const q2a = document.querySelector("#q2a");
   const q2b = document.querySelector("#q2b");
   const q2select = document.querySelector("#q2select");
-  const box = document.querySelector('.q2select_box')
+  const box = document.querySelector(".q2select_box");
 
   q2a.addEventListener("change", () => {
     if (q2a.checked) {
       q2select.setAttribute("disabled", "");
-      box.classList.remove('displayBox')
+      box.classList.remove("displayBox");
     }
   });
 
   q2b.addEventListener("change", () => {
     if (q2b.checked) {
-        box.classList.add('displayBox')
+      box.classList.add("displayBox");
       q2select.removeAttribute("disabled");
     }
   });
@@ -25,103 +23,99 @@ function querySchool() {
   const q3a = document.querySelector("#q3a");
   const q3b = document.querySelector("#q3b");
   const schoolBox = document.querySelector(".school_info");
-  const important = document.querySelector('.important_data');
+  const important = document.querySelector(".important_data");
 
-  const schoolNr = document.getElementById("schoolNumber")
-  const schoolName = document.getElementById("schoolName")
-  const schoolTown = document.getElementById('schoolTown')
+  const schoolNr = document.getElementById("schoolNumber");
+  const schoolName = document.getElementById("schoolName");
+  const schoolTown = document.getElementById("schoolTown");
 
   q3a.addEventListener("change", () => {
     if (q3a.checked) {
       schoolBox.classList.remove("displayBox");
       important.classList.remove("displayBox");
 
-      schoolNr.removeAttribute('required')
-      schoolName.removeAttribute('required')
-      schoolTown.removeAttribute('required')
-      
-      schoolNr.required = false
-      schoolName.required = false
-      schoolTown.required = false
+      // schoolNr.removeAttribute('required')
+      // schoolName.removeAttribute('required')
+      // schoolTown.removeAttribute('required')
+
+      schoolNr.required = false;
+      schoolName.required = false;
+      schoolTown.required = false;
+      schoolNr.value = "";
+      schoolName.value = "";
+      schoolTown.value = "";
     }
   });
 
   q3b.addEventListener("change", () => {
     if (q3b.checked) {
       schoolBox.classList.add("displayBox");
-      important.classList.add("displayBox")
+      important.classList.add("displayBox");
 
-      schoolNr.setAttribute('required','')
-      schoolName.setAttribute('required','')
-      schoolTown.setAttribute('required','')
+      // schoolNr.setAttribute('required','')
+      // schoolName.setAttribute('required','')
+      // schoolTown.setAttribute('required','')
 
-      schoolNr.required = true
-      schoolName.required = true
-      schoolTown.required = true
-
+      schoolNr.required = true;
+      schoolName.required = true;
+      schoolTown.required = true;
     }
   });
 }
 
+function queryAdditional() {
+  const pkt1 = document.querySelector("#q1a");
+  const pkt3 = document.querySelector("#q3a");
+  const pkt5 = document.querySelector("#q5a");
+  const box = document.querySelector(".insurance_box");
 
+  const noCase = document.getElementById("insuranceNo");
+  const yesCase = document.getElementById("insuranceYes");
 
-function queryAdditional(){
-    const pkt1 = document.querySelector('#q1a');
-    const pkt3 = document.querySelector('#q3a');
-    const pkt5 = document.querySelector('#q5a');
-    const box = document.querySelector('.insurance_box');
-
-    const no = document.getElementById("insuranceNo")
-    const yes = document.getElementById("insuranceYes")
-
-    document.addEventListener('change',()=>{
-        if(pkt1.checked && pkt3.checked && pkt5.checked){
-           
-            box.classList.add('displayBox')
-            no.setAttribute('required','')
-            yes.setAttribute('required','')
-            no.required = true
-            yes.required = true
-
-
-        }else{
-            box.classList.remove('displayBox')
-            no.removeAttribute('required')
-            yes.removeAttribute('required')
-            no.required = false
-            yes.required = false;
-        }
-    })
+  document.addEventListener("change", () => {
+    if (pkt1.checked && pkt3.checked && pkt5.checked) {
+      box.classList.add("displayBox");
+      noCase.setAttribute("required", "");
+      yesCase.setAttribute("required", "");
+      noCase.required = true;
+      yesCase.required = true;
+    } else {
+      box.classList.remove("displayBox");
+      noCase.removeAttribute("required");
+      yesCase.removeAttribute("required");
+      noCase.required = false;
+      yesCase.required = false;
+      noCase.checked = false;
+      yesCase.checked = false;
+    }
+  });
 }
 
-function paymentBox(){
-    const payment = document.querySelector('#paymentYes');
-    const paymentFalse = document.querySelector('#paymentNo')
-    const box = document.querySelector('.payment_box')
-    const paymentRequired = document.getElementById("paymentAccountInput")
-    paymentFalse.addEventListener('change',()=>{
-        if(paymentFalse.checked){
-            box.classList.remove('displayBox')
-            paymentRequired.removeAttribute('required')
-            paymentRequired.required = false;
-        }
-    })
+function paymentBox() {
+  const payment = document.querySelector("#paymentYes");
+  const paymentFalse = document.querySelector("#paymentNo");
+  const box = document.querySelector(".payment_box");
+  const paymentRequired = document.getElementById("paymentAccountInput");
+  paymentFalse.addEventListener("change", () => {
+    if (paymentFalse.checked) {
+      box.classList.remove("displayBox");
+      // paymentRequired.removeAttribute('required')
+      paymentRequired.required = false;
+      paymentRequired.value = ""
+    }
+  });
 
-    payment.addEventListener('change',()=>{
-        if(payment.checked){
-            box.classList.add('displayBox');
-            paymentRequired.setAttribute('required','')
-            paymentRequired.required = true;
-        }
-    })
+  payment.addEventListener("change", () => {
+    if (payment.checked) {
+      box.classList.add("displayBox");
+      // paymentRequired.setAttribute('required','')
+      paymentRequired.required = true;
+    }
+  });
 }
 
-
-
-function validateName(nameInput,errorMessage){
-   
-
-  nameInput.addEventListener("input", function() {
+function validateName(nameInput, errorMessage) {
+  nameInput.addEventListener("input", function () {
     var inputName = nameInput.value.trim();
     var hasError = false;
 
@@ -150,10 +144,8 @@ function validateName(nameInput,errorMessage){
   });
 }
 
-function validatePesel(peselInput,errorMessage){
-
-
-  peselInput.addEventListener("input", function() {
+function validatePesel(peselInput, errorMessage) {
+  peselInput.addEventListener("input", function () {
     var inputPesel = peselInput.value.replace(/\s/g, ""); // Usuwanie białych znaków
     var hasError = false;
 
@@ -183,9 +175,9 @@ function validatePesel(peselInput,errorMessage){
 }
 
 function validatePhone(input, errorDiv) {
-   // Pobranie wartości z pola tekstowego i usunięcie ewentualnych białych znaków
+  // Pobranie wartości z pola tekstowego i usunięcie ewentualnych białych znaków
 
-  input.addEventListener("input",()=>{
+  input.addEventListener("input", () => {
     const phoneNumber = input.value.trim();
     const phoneRegex = /^(\+\d{1,3}\s?)?\d{1,3}(\s?\d{3}){2,3}$/;
 
@@ -196,20 +188,18 @@ function validatePhone(input, errorDiv) {
     } else {
       input.classList.remove("error");
       input.classList.add("success");
-      errorDiv.textContent  = "";
+      errorDiv.textContent = "";
     }
-  })
-  
-   // Wyrażenie regularne do sprawdzenia formatu numeru telefonu
+  });
 
+  // Wyrażenie regularne do sprawdzenia formatu numeru telefonu
 }
 
 function validateEmail(input, errorMessage) {
-
-  input.addEventListener('input',()=>{
+  input.addEventListener("input", () => {
     var email = input.value.trim();
     var hasError = false;
-  
+
     // Sprawdzenie, czy pole tekstowe jest puste
     if (email === "") {
       hasError = true;
@@ -222,7 +212,7 @@ function validateEmail(input, errorMessage) {
         errorMessage.textContent = "Podaj poprawny adres e-mail.";
       }
     }
-  
+
     // Jeśli nie ma błędów, ustaw styl poprawny
     if (!hasError) {
       input.classList.remove("error");
@@ -233,18 +223,16 @@ function validateEmail(input, errorMessage) {
       input.classList.add("error");
       errorMessage.style.display = "block";
     }
-  })
-
+  });
 }
 
 function validatePostalCode(input, errorDiv) {
-
-  input.addEventListener('input',()=>{
+  input.addEventListener("input", () => {
     const postalCode = input.value.trim(); // Pobranie wartości z pola tekstowego i usunięcie ewentualnych białych znaków
 
     // Wyrażenie regularne do sprawdzenia formatu kodu pocztowego
     const postalCodeRegex = /^(\d{2}-\d{3}|\d{2}\s?\d{3}|\d{5})$/;
-  
+
     if (!postalCodeRegex.test(postalCode)) {
       input.classList.remove("success");
       input.classList.add("error");
@@ -254,23 +242,77 @@ function validatePostalCode(input, errorDiv) {
       input.classList.add("success");
       errorDiv.innerText = "";
     }
-  })
-
+  });
 }
 
+function validatePassData() {
+  const inputs = document.querySelectorAll(".checkID");
+  const btn = document.querySelector(".submitBtn");
+  const pass = document.getElementById("pass");
 
-function validateAllInputs(){
-  validateName(document.getElementById("name"),document.getElementById("nameError"));
-  validateName(document.getElementById("surname"),document.getElementById("surnameError"));
-  validateName(document.getElementById("post"),document.getElementById("postError"));
-  validateName(document.getElementById("surname"),document.getElementById("surnameError"));
-  validateName(document.getElementById("surname"),document.getElementById("surnameError"));
-  validatePesel(document.getElementById('pesel'),document.getElementById('peselError'))
-  validatePhone(document.getElementById('phone'),document.getElementById('phoneError'))
-  validateEmail(document.getElementById('email'),document.getElementById('emailError'))
-  validatePostalCode(document.getElementById('postcode'),document.getElementById('postcodeError'))
-  validateName(document.getElementById("street"),document.getElementById("streetError"))
-  validateName(document.getElementById("state"),document.getElementById("stateError"))
+  document.addEventListener("change", () => {
+    let temp = 0;
+    for (const el of inputs) {
+      if (el.classList.contains("error")) {
+        btn.setAttribute("disabled", "");
+      }
+      if (el.classList.contains("success")) {
+        temp++;
+        if (temp === 9) {
+          btn.removeAttribute("disabled");
+        }
+      }
+    }
+  });
+}
+
+function validateAllInputs() {
+  validateName(
+    document.getElementById("name"),
+    document.getElementById("nameError")
+  );
+  validateName(
+    document.getElementById("surname"),
+    document.getElementById("surnameError")
+  );
+  validateName(
+    document.getElementById("post"),
+    document.getElementById("postError")
+  );
+  validateName(
+    document.getElementById("surname"),
+    document.getElementById("surnameError")
+  );
+  validateName(
+    document.getElementById("surname"),
+    document.getElementById("surnameError")
+  );
+  validatePesel(
+    document.getElementById("pesel"),
+    document.getElementById("peselError")
+  );
+  validatePhone(
+    document.getElementById("phone"),
+    document.getElementById("phoneError")
+  );
+  validateEmail(
+    document.getElementById("email"),
+    document.getElementById("emailError")
+  );
+  validatePostalCode(
+    document.getElementById("postcode"),
+    document.getElementById("postcodeError")
+  );
+  validateName(
+    document.getElementById("street"),
+    document.getElementById("streetError")
+  );
+  validateName(
+    document.getElementById("state"),
+    document.getElementById("stateError")
+  );
+
+  validatePassData();
 }
 
 queryInvalid();
